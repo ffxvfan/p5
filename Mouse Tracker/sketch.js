7958b876281b2df69d186acbb2b271722ad6bc94
ffxvfan <ffxvfan@gmail.com>
2017-08-05 22:51:10 -0400

    alpha

--- Mouse Tracker/sketch.js
+++ Mouse Tracker/sketch.js
@@ -1,11 +1,11 @@
 function setup() {
   createCanvas(800, 600);
+    background(0);
 }
 
 function draw() {
-  noStroke();
-  fill(255, 255, 255, 20);
-  ellipse(mouseX, mouseY, 100, 100);
   background(0);
-  // testing, testing
+  noStroke();
+  fill(255, 255, 255);
+  ellipse(mouseX, mouseY, 100, 100);
 }
