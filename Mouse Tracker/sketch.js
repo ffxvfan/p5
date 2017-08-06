3e3698236f233a867d63ba8f42fee82fd3c16247
ffxvfan <ffxvfan@gmail.com>
2017-08-05 22:48:28 -0400

    new frames

--- Mouse Tracker/sketch.js
+++ Mouse Tracker/sketch.js
@@ -1,10 +1,10 @@
 function setup() {
   createCanvas(800, 600);
-  background(0);
 }
 
 function draw() {
   noStroke();
-  fill(255, 255, 255, 10);
-  ellipse(mouseX, mouseY, 200, 300);
+  fill(255, 255, 255, 20);
+  ellipse(mouseX, mouseY, 100, 100);
+  background(0);
 }
