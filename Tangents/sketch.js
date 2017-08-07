8cdad4ec4cf52f9b3004db88847bb080668fc356
ffxvfan <ffxvfan@gmail.com>
2017-08-07 12:04:13 -0400

    added sliders

--- Tangents/sketch.js
+++ Tangents/sketch.js
@@ -1,19 +1,32 @@
+var iteration;
+var iterationSlider, zoomSlider, axisCheckbox;
+
 function setup() {
   createCanvas(1000, 800);
+  //slider descriptions & position
+  iterationSlider = createSlider(0.1, 20, 1, 0.1);
+  iterationSlider.position(20, 20);
+
+  zoomSlider = createSlider(0.2, 5, 1, 0.1);
+  zoomSlider.position(20, 60);
+
+  axisCheckbox = createCheckbox("Show Axis", true);
+  // axisCheckbox.changed(myCheckedEvent);
+  axisCheckbox.position(20, 100);
 }
 
 function f(x) {
-  return sin(3*x);
+  return x*x*x-x;
 }
 
 function fPrime(x) {
-
-  return 3*cos(3*x);
+  var h = 0.0001;
+  return (f(x+h)-f(x))/h;
 }
 
-
 function draw() {
-  var pixelsPerUnit = 100.0;
+  push();
+  var pixelsPerUnit = 100.0 * zoomSlider.value();
   var xStepSize = 0.005;
   var xMin = -width / pixelsPerUnit / 2;
   var xMax = width / pixelsPerUnit / 2;
@@ -26,27 +39,42 @@ function draw() {
 
   strokeWeight(1.0 / pixelsPerUnit);
 
-  fill(0);
+  iteration = iterationSlider.value();
 
   //draw axis
-  stroke(0, 40);
-  line(xMin, 0, xMax, 0);
-  line(0, yMin, 0, yMax);
+  if (axisCheckbox.checked()) {
+    stroke(0, 40);
+    line(xMin, 0, xMax, 0);
+    line(0, yMin, 0, yMax);
+  }
 
   //graph parabola
-  // stroke(0);
-  // noFill();
-  // beginShape();
-  // for(var x = xMin-xStepSize; x < xMax+2*xStepSize; x += xStepSize) {
-  //   curveVertex(x, f(x));
-  // }
-  // endShape();
+  stroke(0);
+  noFill();
+  beginShape();
+  for (var x = xMin - xStepSize; x < xMax + 2 * xStepSize; x += xStepSize) {
+    curveVertex(x, f(x));
+  }
+  endShape();
 
   //graph tangents
   stroke(0, 10);
-  for(var x = xMin-xStepSize; x < xMax+2*xStepSize; x += xStepSize) {
-    line(xMin, fPrime(x)*(xMin - x) + f(x), xMax, fPrime(x)*(xMax - x) + f(x));
-
+  for (var x = xMin - xStepSize; x < xMax + 2 * xStepSize; x += xStepSize) {
+    line(xMin, fPrime(x) * (xMin - x) + f(x), xMax, fPrime(x) * (xMax - x) + f(x));
   }
+  pop();
+
+  //UI labels
+  text("Frequency", 20, 20);
+  text("Zoom", 20, 60);
+
 
 }
+
+// function myCheckedEvent() {
+//   if (this.checked()) {
+//     console.log("Checking!");
+//   } else {
+//     console.log("Unchecking!");
+//   }
+// }
